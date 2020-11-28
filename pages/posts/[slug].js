4b4ccc9da1a411c2bd3fe34837e6f467e801e01d
Cody Olsen <cody.olsen@finn.no>
2020-11-29 00:34:28 +0100

    no more comments

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -5,14 +5,12 @@ import PostBody from '../../components/post-body'
 import MoreStories from '../../components/more-stories'
 import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
-import Comments from '../../components/comments'
 import SectionSeparator from '../../components/section-separator'
 import Layout from '../../components/layout'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
-import Form from '../../components/form'
 
 export default function Post({ post, morePosts, preview }) {
   const router = useRouter()
@@ -43,9 +41,6 @@ export default function Post({ post, morePosts, preview }) {
               <PostBody content={post.body} />
             </article>
 
-            <Comments comments={post.comments} />
-            <Form _id={post._id} />
-
             <SectionSeparator />
             {morePosts.length > 0 && <MoreStories posts={morePosts} />}
           </>
@@ -63,7 +58,7 @@ export async function getStaticProps({ params, preview = false }) {
       post: data?.post || null,
       morePosts: data?.morePosts || null,
     },
-    revalidate: 1
+    revalidate: 1,
   }
 }
 
