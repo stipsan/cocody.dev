4c1fcec624cd7aafc2f3cc05888abb26aae2ec34
Cody Olsen <stipsan@gmail.com>
2020-11-30 17:18:21 +0100

    setup analytics (#16)

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const ga = process.env.NEXT_PUBLIC_GA
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {ga && (
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${ga}`}
+            />
+          )}
+          {ga && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${ga}');
+          `,
+              }}
+            />
+          )}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Carter+One&text=Cody%20Olsen"
